877c84c125271b7a7cfb33f78a4198a2c48344cc
FOLKQQ <fbifolkcom2@hotmail.com>
2018-08-13 12:24:59 +0700

    งาน 14ส.ค.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,16 +1,18 @@
 package controllers;
 
 import models.Shirts;
-import play.*;
 import play.api.templates.Html;
-import play.mvc.*;
-
+import play.data.DynamicForm;
+import play.data.Form;
+import play.mvc.Controller;
+import play.mvc.Result;
 import views.html.*;
 
 public class Application extends Controller {
 
     public static Result main(Html data){
         return ok(main.render(data));
+
 
     }
     public static Result showMyShirts() {
@@ -27,23 +29,41 @@ public class Application extends Controller {
         Shirts co = new Shirts("C004","เสื้อขนสัตวื","Allz","น้ำเงิน",11800.00,5);
         Shirts v = new Shirts("V005","เสื้อกล้าม","Uniqlo","ชมพู",300.00,10);
         Shirts b = new Shirts("B006","เสื้อทั่วไป","AA","ม่วง",100.00,20);
-
         return main(showMyShirts.render(j,d,jk,co,v,b));
-
-
-
-
     }
+    public  static  Result inputShirts(){
+        return main (inputShirts.render());
+    }
+    public static Result postShirts() {
+        Shirts jjj =new Shirts();
+        DynamicForm show= Form.form().bindFromRequest();
+        String id,name,logo,col;
+        double num,price;
+        if(show.hasErrors()){
+            return main(inputShirts.render());
 
+        }else {
+            id = show.get("id");
+            name = show.get("name");
+            logo = show.get("logo");
+            col = show.get("col");
+            num = Double.parseDouble(show.get("num"));
+            price = Double.parseDouble(show.get("price"));
 
-
+        }
+         jjj.setId(id);
+        jjj.setName(name);
+        jjj.setLogo(logo);
+        jjj.setPrice(price);
+        jjj.setNum(num);
+        jjj.setCol(col);
+        return main(showOuputShirts.render(jjj));
+    }
 
     public static Result home(){
         return main(home.render());
     }
 
 
-    public static Result footer(){
-        return main(footer.render());
-    }
+
 }
